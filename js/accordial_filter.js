102fdfc86e765883f283e5d210d31678aed4efc0
javadragan <javadragan@gmail.com>
2017-05-12 23:39:28 +0200

    Basic template files

---
+++ js/accordial_filter.js
@@ -0,0 +1,44 @@
+$('.filter1').on('click', function(){
+    $(this).toggleClass('active');
+});
+
+
+
+
+
+
+$(".id0").on("click",function(e){
+    $(".id-0").trigger("click");
+    e.preventDefault();
+});
+
+
+$(".id1").on("click",function(e){
+    $(".id-1").trigger("1click");
+    e.preventDefault();
+});
+
+
+$(".id2").on("click",function(e){
+    $(".id-2").trigger("click");
+    e.preventDefault();
+});
+
+
+$(".id3").on("click",function(e){
+    $(".id-3").trigger("click");
+    e.preventDefault();
+});
+
+
+$(".id4").on("click",function(e){
+    $(".id-4").trigger("click");
+    e.preventDefault();
+});
+
+$(".id5").on("click",function(e){
+    $(".id-5").trigger("click");
+    e.preventDefault();
+});
+
+
